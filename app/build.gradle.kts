918e3ad7268dc942b0a98a935659e6e302fe9921
Bhat Muminn <144714960+Bhat-mumin@users.noreply.github.com>
2026-09-17 11:50:41 +0530

    Update Java and Kotlin compatibility to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,13 @@ android {
         targetSdk = 35
         versionCode = 2
         versionName = "1.0"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
